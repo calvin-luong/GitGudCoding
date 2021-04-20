60a76a94af8e840e57d766f04b8d30cde70b0e15
Calvin Luong <calvin.luong@sjsu.edu>
2021-04-19 21:45:54 -0700

    cleaning up landing page

--- src/components/routes/navbar.js
+++ src/components/routes/navbar.js
@@ -21,6 +21,16 @@ const NavBar = () => {
               Videos
             </a>
           </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/searchQuestion">
+              Coding Questions
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/searchTutorial">
+              Coding Tutorials
+            </a>
+          </li>
         </ul>
       </div>
       <div class="navbar-collapse collapse w-100 order-3 dual-collapse2">
