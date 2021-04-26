01196daf170cd1b5d8d55d30d578e06c24145039
Calvin Luong <calvin.luong@sjsu.edu>
2021-04-25 20:26:35 -0700

    button for video discussion

--- src/app.js
+++ src/app.js
@@ -1,35 +1,37 @@
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
-import SignIn from './components/routes/signin';
-import SignUp from './components/routes/signup';
-import SearchVid from './components/routes/searchVid';
-import Landing from './components/landing';
-import Discussion from './components/routes/discussion';
-import Post from './components/routes/post';
-import CreatePost from './components/routes/createPost';
+import SignIn from "./components/routes/signin";
+import SignUp from "./components/routes/signup";
+import SearchVid from "./components/routes/searchVid";
+import Landing from "./components/landing";
+import Discussion from "./components/routes/discussion";
+import Post from "./components/routes/post";
+import CreatePost from "./components/routes/createPost";
+import CreatePostWVideo from "./components/routes/createPostWithVideo";
 
 function App() {
-   return (
-      <Router>
-         <div className="App">
-            <div className="auth-wrapper">
-               <div className="auth-inner">
-                  <Switch>
-                     <Route exact path="/" component={Landing} />
-                     <Route path="/searchVid" component={SearchVid} />
-                     <Route path="/login" component={SignIn} />
-                     <Route path="/sign-up" component={SignUp} />
-                     <Route path="/discussion" component={Discussion} />
-                     <Route path="/post" component={Post} />
-                     <Route path="/create-post" component={CreatePost} />
-                  </Switch>
-               </div>
-            </div>
-         </div>
-      </Router>
-   );
+  return (
+    <Router>
+      <div className="App">
+        <div className="auth-wrapper">
+          <div className="auth-inner">
+            <Switch>
+              <Route exact path="/" component={Landing} />
+              <Route path="/searchVid" component={SearchVid} />
+              <Route path="/login" component={SignIn} />
+              <Route path="/sign-up" component={SignUp} />
+              <Route path="/discussion" component={Discussion} />
+              <Route path="/post" component={Post} />
+              <Route path="/create-post" component={CreatePost} />
+              <Route path="/create-post-video" component={CreatePostWVideo} />
+            </Switch>
+          </div>
+        </div>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
--- src/components/routes/video.js
+++ src/components/routes/video.js
@@ -1,24 +1,50 @@
 import React, { Component } from "react";
-import ReactPlayer from "react-player/youtube";
+import { withRouter } from "react-router-dom";
 
 class video extends Component {
   state = {};
+
+  constructor(props) {
+    super(props);
+    this.discussVid = this.discussVid.bind(this);
+  }
+
+  discussVid() {
+    this.props.history.push("/create-post-video", {
+      videoID: "https://www.youtube.com/embed/" + this.props.urlInput,
+    });
+  }
 
   render() {
     const urlString = "https://www.youtube.com/embed/" + this.props.urlInput;
 
     return (
-      <iframe
-        width="560"
-        height="315"
-        src={urlString}
-        title="YouTube video player"
-        frameborder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowfullscreen
-      />
+      <div>
+        <iframe
+          width="560"
+          height="315"
+          src={urlString}
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        />
+        <br></br>
+        <a
+          role="button"
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+          class="btn btn-outline-primary"
+          onClick={this.discussVid}
+        >
+          Create Discussion
+        </a>
+      </div>
     );
   }
 }
 
-export default video;
+export default withRouter(video);
